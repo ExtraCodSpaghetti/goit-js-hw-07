c2ec4898cac6ea835ea0c7d4b7ad0e6de5c29c93
Kostiantyn Chesak <K03.chesak@gmail.com>
2025-02-19 00:36:41 +0100

    6

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const loginForm = document.querySelector(".login-form");
 
+  loginForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const {
+      email: { value: email },
+      password: { value: password },
+    } = loginForm.elements;
+
+    if (!email.trim() || !password.trim()) {
+      alert("All form fields must be filled in");
+      return;
+    }
+
+    const formData = {
+      email: email.trim(),
+      password: password.trim(),
+    };
+
+    console.log(formData);
+
+    loginForm.reset();
+  });
+});
